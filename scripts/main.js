c87fe179961d2053ace88821f1f835c3be15fd94
Tim Chu <tcchu97@gmail.com>
2019-11-21 19:19:41 -0500

    loop working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-const {setInterval} = require('timers');
+const {
+  setInterval
+} = require('timers');
 const admin = require('firebase-admin');
 const TEN_SECONDS = 10000;
 const ONE_SECOND = 1000;
@@ -20,167 +22,159 @@ var inGame = false;
 
 
 var fullGameState = {
-    var: currentDeck = "",
-    /*null or (dictionary) */
-    var: gameState = "Menus",
-    /*Menus or InProgress*/
-    var: currentUser = "",
-    /*null or (username)*/
-    var: prevGameID = "",
-    var: GameID = "",
-    /*-1 or (id)*/
+  currentDeck: "",
+  /*null or (dictionary) */
+  gameState: "",
+  /*Menus or InProgress*/
+  PlayerName: "",
+  /*null or (username)*/
+  prevGameID: "",
+  GameID: "",
+  /*-1 or (id)*/
 };
 
 var validGame = false;
 var recruitChallenge = {
-    "01PZ008": 3
+  "01PZ008": 3
 };
 
-async function getCurrentUser() {
-    try {
-        const response = await axios.get('http://localhost:21337/positional-rectangles');
-        console.log(response.data);
-        return response.data.PlayerName;
-    } catch (error) {
-        console.error(error);
-    }
-} 
+async function getPlayerName() {
+  try {
+    const response = await axios.get('http://localhost:21337/positional-rectangles');
+    console.log(response.data);
+    return response.data.PlayerName;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getActiveDeck() {
-    try {
-        const response = await axios.get('http://localhost:21337/static-decklist');
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-}   
+  try {
+    const response = await axios.get('http://localhost:21337/static-decklist');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getGameState() {
-    try {
-        const response = await axios.get('http://localhost:21337/positional-rectangles');
-        console.log(response.data.GameState);
-        return response.data.GameState;
-    } catch (error) {
-        console.error(error);
-    }
-} 
+  try {
+    const response = await axios.get('http://localhost:21337/positional-rectangles');
+    console.log(response.data.GameState);
+    return response.data.GameState;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getLastGameResult() {
-    try {
-        const response = await axios.get('http://localhost:21337/game-result');
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-} 
+  try {
+    const response = await axios.get('http://localhost:21337/game-result');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function checkRestrictions() {
-    if (!fullGameState.currentDeck) {
+  if (!fullGameState.currentDeck) {
+    return false;
+  }
+  for (var card in recruitChallenge) {
+    console.log(fullGameState.currentDeck[card] + " " + recruitChallenge[card]);
+    if (fullGameState.currentDeck[card] < recruitChallenge[card]) {
       return false;
     }
-    for (var card in recruitChallenge) {
-        console.log(fullGameState.currentDeck[card] + " " + recruitChallenge[card]);
-        if (fullGameState.currentDeck[card] < recruitChallenge[card]) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 function updatePlayerWinLoss() {
-    if(!fullGameState.currentUser){
-      return;
-    }
-    let userRef = userColRef.doc(fullGameState.currentUser);
-    let historyRef = userRef.collection("History");
-    historyRef.where('Result', '==', true).get()
-      .then(history => {
-        if (history.empty) {
-          console.log('No match history found');
-          return;
-        }
-
-        userRef.set({
-          Wins: snapshot.size
-        }, {
-          merge: true
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      });
-    historyRef.where('Result', '==', false).get()
-      .then(history => {
-        if (history.empty) {
-          console.log('No match history found');
-          return;
-        }
-
-        userRef.set({
-          Losses: history.size
-        }, {
-          merge: true
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      });
+  if (!fullGameState.PlayerName) {
+    return;
   }
+  var userRef = userColRef.doc(fullGameState.PlayerName);
+  var historyRef = userRef.collection("History");
+  historyRef.where('Result', '==', true).get()
+    .then(history => {
+      if (history.empty) {
+        console.log('No match history found');
+        return;
+      }
 
-async function updateGameState() {
-    var gameState = await getGameState();
-    fullGameState.gamestate = gameState ? gameState : fullGameState.gamestate;
-    
-    var currentUser = await getCurrentUser();
-    fullGameState.currentUser = currentUser ? currentUser : fullGameState.currentUser;
-    
-    var currentDeck = await getActiveDeck();
-    fullGameState.currentDeck = currentDeck ? currentDeck : fullGameState.currentDeck;    
-    
-    var gameId = await getLastGameResult();
-    fullGameState.GameID = gameId;
+      userRef.set({
+        Wins: snapshot.size
+      }, {
+        merge: true
+      });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+  historyRef.where('Result', '==', false).get()
+    .then(history => {
+      if (history.empty) {
+        console.log('No match history found');
+        return;
+      }
+
+      userRef.set({
+        Losses: history.size
+      }, {
+        merge: true
+      });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
 }
 
 async function mainLoop() {
-    //while app is open
-    //check if game is in progress
-    //if game in progress
-    //get username
-    //check if username exists in db
-        //if it doesnt, push new entry to db
-        //if it does, get db entry ref
-    //check until game is done (not in progress)
-    //update win/loss
-    
-    let gameState = await getGameState();
-    fullGameState.gamestate = gameState ? gameState : fullGameState.gamestate;
-    // game is in progress, set flag to do processing after
-    if(fullGameState.gameState === 'InProgress' && inGame){
-        let currentUser = await getCurrentUser();
-        fullGameState.currentUser = currentUser ? currentUser : fullGameState.currentUser;
-        let currentDeck = await getActiveDeck();
-        fullGameState.currentDeck = currentDeck ? currentDeck : fullGameState.currentDeck;    
-        validGame = checkRestrictions();
-        inGame = true;
+  //while app is open
+  //check if game is in progress
+  //if game in progress
+  //get username
+  //check if username exists in db
+  //if it doesnt, push new entry to db
+  //if it does, get db entry ref
+  //check until game is done (not in progress)
+  //update win/loss
+
+
+  var gameState = await getGameState();
+  fullGameState.gameState = gameState ? gameState : fullGameState.gameState;
+  console.log("pls");
+  // game is in progress, set flag to do processing after
+  if (fullGameState.gameState === 'InProgress' && inGame === false) {
+    inGame = true;
+    var playerName = await getPlayerName();
+    fullGameState.PlayerName = playerName ? playerName : fullGameState.PlayerName;
+    var currentDeck = await getActiveDeck();
+    fullGameState.currentDeck = currentDeck ? currentDeck : fullGameState.currentDeck;
+    validGame = checkRestrictions();
+    console.log("in loop");
+  }
+  console.log(JSON.stringify(fullGameState));
+  // game has ended, but we have to do stuff on our end
+  if (fullGameState.gameState === 'Menus' && inGame === true) {
+    inGame = false;
+    var gameResult = await getLastGameResult();
+    console.log(gameResult);
+    if (validGame) {
+      var resultData = {
+        GameID: gameResult.GameID,
+        Result: gameResult.LocalPlayerWon
+      }
+      var setDoc = userColRef
+        .doc(fullGameState.PlayerName).collection('History');
+      setDoc.add(resultData).then(documentReference => {
+        updatePlayerWinLoss();
+      });
     }
-    // game has ended, but we have to do stuff on our end
-    if(fullGameState.gameState === 'Menus' && inGame){
-        let gameResult = getLastGameResult();
-        if(validGame) {
-            let resultData = {
-                GameID: gameResult.GameId,
-                Result: gameResult.LocalPlayerWon
-            }
-            let setDoc = userColRef
-                .doc(fullGameState.PlayerName).collection('History');
-            setDoc.add(resultData).then(documentReference => {
-                updatePlayerWinLoss();
-            });
-        }
-    }
-    fullGameState.gamestate = await getGameState();
+
+  }
 }
 
-setInterval(mainLoop, ONE_SECOND*5);
+setInterval(mainLoop, ONE_SECOND * 5);
